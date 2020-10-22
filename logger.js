46aaa5b8081a5508aebc6598494a1104e5abbee5
Oleg Chaichuk <olegchaichuk@team.asana.com>
2020-10-22 15:10:35 +0300

    Clear master

--- logger.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    log : (message) => {console.log(message)},
-    warn : (message) => {console.log('[warn] ' + message)},
-    error : (message) => {console.log('[error] ' + message)},
-    debug : (message) => {console.log('[debug] ' + message)}
-}
