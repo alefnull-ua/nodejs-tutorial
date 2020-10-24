5483506a39d4126acc3f0c6a929846e11487cf92
alefnull-ua <chaichuko@gmail.com>
2020-10-24 10:24:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,9 @@
-module.exports.PORT = process.env.PORT;
-
 const argEnv = process.argv.find((value) => {return value.startsWith('-env=')})
 let env = "development"
 if (argEnv != undefined) {
     env = argEnv.replace('-env=', '')
 };
-module.exports.ENV = env;
+module.exports = {
+    ENV: env,
+    PORT: process.env.PORT
+}
