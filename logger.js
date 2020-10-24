5b165ce49abaef5d8ccc67f8da820d731f70a3d5
alefnull-ua <chaichuko@gmail.com>
2020-10-24 10:25:26 +0300

    Update logger.js

--- logger.js
+++ logger.js
@@ -1,6 +1,6 @@
 module.exports = {
     log : (message) => {console.log(message)},
-    warn : (message) => {console.log('[warn] ' + message)},
-    error : (message) => {console.log('[error] ' + message)},
-    debug : (message) => {console.log('[debug] ' + message)}
+    warn : (message) => {console.warn(message)},
+    error : (message) => {console.error(message)},
+    debug : (message) => {console.debug(message)}
 }
